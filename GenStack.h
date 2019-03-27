fea1a9037c4c76ccb6873bd03a759dddc365de5d
brentjang <jang119@mail.chapman.edu>
2019-03-27 01:33:40 +0000

    seventh commit

--- GenStack.h
+++ GenStack.h
@@ -12,94 +12,111 @@ private:
     T *tempArray;
 
 public:
-    GenStack()
-    {
-        //default constructor for stack of type T
-        myArray = new T[100];
-        size = 100;
-        top = -1;
-    }
-
-    GenStack(int maxSize)
-    {
-        myArray = new T[maxSize];
-        size = maxSize;
-        top = -1;
-    }
-
-    ~GenStack()
-    {
-        delete myArray;
-        delete tempArray;
-    }
-
-    void push(T d)
-    {
-        if(isFull())
-        {
-            //if stack is full, copy to new stack and increase, then copy back to old stack
-            int oldSize = size;
-            size += 1;
-
-            tempArray = new T[size];
-            for(int i = 0; i < oldSize; i++)
-            {
-                tempArray[i] = myArray[i];
-            }
-
-            myArray = new T[size];
-            for(int i = 0; i < oldSize; i++)
-            {
-                myArray[i] = tempArray[i];
-            }
-
-            push(d);
-        }
-        else
-        {
-            myArray[++top] = d;
-        }
-    }
-
-    T pop()
-    {
-        try
-        {
-            if(isEmpty())
-            {
-                throw InvalidStackOperationException("Cannot pop an empty stack.");
-            }
-        }
-        catch (InvalidStackOperationException& msg)
-        {
-            cout << msg.getMessage() << endl;
-        }
-        return myArray[top--];
-    }
-
-    T peek()
-    {
-        try
-        {
-            if(isEmpty())
-            {
-                throw InvalidStackOperationException("Cannot peek an empty stack.");
-            }
-        }
-        catch (InvalidStackOperationException& msg)
-        {
-            cout << msg.getMessage() << endl;
-        }
-        return myArray[top];
-    }
-
-    bool isFull()
-    {
-        return (top == size-1);
-    }
-
-    bool isEmpty()
-    {
-        return (top == -1);
-    }
+    GenStack();
+    GenStack(int maxSize);
+    ~GenStack();
+    void push(T d);
+    T pop();
+    T peek();
+    bool isFull();
+    bool isEmpty();
 };
+
+template<class T>
+GenStack<T>::GenStack()
+{
+    //default constructor for stack of type T
+    myArray = new T[100];
+    size = 100;
+    top = -1;
+}
+
+template<class T>
+GenStack<T>::GenStack(int maxSize)
+{
+    myArray = new T[maxSize];
+    size = maxSize;
+    top = -1;
+}
+
+template<class T>
+GenStack<T>::~GenStack()
+{
+    delete myArray;
+    delete tempArray;
+}
+
+template<class T>
+void GenStack<T>::push(T d)
+{
+    if(isFull())
+    {
+        //if stack is full, copy to new stack and increase, then copy back to old stack
+        int oldSize = size;
+        size += 1;
+
+        tempArray = new T[size];
+        for(int i = 0; i < oldSize; i++)
+        {
+            tempArray[i] = myArray[i];
+        }
+
+        myArray = new T[size];
+        for(int i = 0; i < oldSize; i++)
+        {
+            myArray[i] = tempArray[i];
+        }
+
+        push(d);
+    }
+    else
+    {
+        myArray[++top] = d;
+    }
+}
+
+template<class T>
+T GenStack<T>::pop()
+{
+    try
+    {
+        if(isEmpty())
+        {
+            throw InvalidStackOperationException("Cannot pop an empty stack.");
+        }
+    }
+    catch (InvalidStackOperationException& msg)
+    {
+        cout << msg.getMessage() << endl;
+    }
+    return myArray[top--];
+}
+
+template<class T>
+T GenStack<T>::peek()
+{
+    try
+    {
+        if(isEmpty())
+        {
+            throw InvalidStackOperationException("Cannot peek an empty stack.");
+        }
+    }
+    catch (InvalidStackOperationException& msg)
+    {
+        cout << msg.getMessage() << endl;
+    }
+    return myArray[top];
+}
+
+template<class T>
+bool GenStack<T>::isFull()
+{
+    return (top == size-1);
+}
+
+template<class T>
+bool GenStack<T>::isEmpty()
+{
+    return (top == -1);
+}
